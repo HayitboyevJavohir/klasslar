037b38a78f1006bc149dd813408ec7f42a52f5c1
Javohir <javohir@example.com>
2026-05-21 12:02:26 +0500

    done

--- main.py
+++ main.py
@@ -13,5 +13,70 @@ talaba2 = Talaba("Abbosbek", "Axmedov", 2002)
 talaba3 = Talaba("Olim" "Olimov", 1995)
 talaba4 = Talaba("Husan" "Akbarov", 2004)
 
-talaba3.tanishtir()
-talaba1.tanishtir()
+class Talaba:
+    """Talaba nomli klass yaratamiz"""
+    def __init__(self,ism,familiya,tyil):
+        """Talabaning xususiyatlari"""
+        self.ism = ism
+        self.familiya = familiya
+        self.tyil
+        
+    def get_name(self):
+        """Talabaning ismini qaytaradi"""
+        return self.ism
+    
+    def get_lastname(self):
+        """Talabaning familiyasini qaytaradi"""
+        return self.familiya
+    
+    def get_fullname(self):
+        """Talabaning ism-familiyasini qaytaradi"""
+        return f"{self.ism} {self.familiya}"
+    
+    def tanishtir(self):
+        print(f"Ismim {self.ism} {self.familiya}. {self.tyil} yilda tu'gilganman")
+
+talaba1 = Talaba("Alijon","Valiyev",2000)
+print(talaba1.get_fullname()) 
+
+class Talaba:
+    """Talaba nomli klass yaratamiz"""
+    def __init__(self,ism,familiya,tyil):
+        """Talabaning xususiyatlari"""
+        self.ism = ism
+        self.familiya = familiya
+        self.tyil = tyil
+    
+    def get_name(self):
+        """Talabaning ismini qaytaradi"""
+        return self.ism
+    
+    def get_lastname(self):
+        """Talabaning familiyasini qaytaradi"""
+        return self.familiya
+    
+    def get_fullname(self):
+        """Talabaning ism-familiyasini qaytaradi"""
+        return f"{self.ism} {self.familiya}"
+    
+    def get_age(self,yil):
+        """Talabaning yoshini qaytaradi"""
+        return yil-self.tyil
+    
+    def tanishtir(self):
+        print(f"Ismim {self.ism} {self.familiya}. {self.tyil} yilda tu'gilganman")
+
+    talaba1 = Talaba("Alijon","Valiyev",2000)
+print(talaba1.get_age(2021))
+
+class User:
+    def __int__(self,name,username,email):
+        self.name = name
+        self.uname = username
+        self.mail = email
+    
+    def describe():
+        pass
+    
+    def get_email():
+        pass
