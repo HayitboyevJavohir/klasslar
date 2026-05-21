cc9671c5c5f2528960a3809a1e9c9df83da26366
Javohir <javohir@example.com>
2026-05-21 11:37:21 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,7 @@
+class Talaba:
+    """Talaba nomli klass yaratamiz"""
+    def __init__(self,ism,familiya,tyil):
+        """Talabaning xususiyatlari"""
+        self.ism = ism
+        self.familiya = familiya
+        self.tyil = tyil
