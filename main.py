e719023105d6b81fba356ca96be24bf674130e6f
Javohir <javohir@example.com>
2026-05-21 11:49:27 +0500

    done

--- main.py
+++ main.py
@@ -5,3 +5,13 @@ class Talaba:
         self.ism = ism
         self.familiya = familiya
         self.tyil = tyil
+    def tanishtir(self):
+        print(f"Ismim {self.ism} {self.familiya}. {self.tyil} yilda tugilganman")
+
+talaba1 = Talaba("Javohir", "Hayitboyev", 2011)
+talaba2 = Talaba("Abbosbek", "Axmedov", 2002)
+talaba3 = Talaba("Olim" "Olimov", 1995)
+talaba4 = Talaba("Husan" "Akbarov", 2004)
+
+talaba3.tanishtir()
+talaba1.tanishtir()
